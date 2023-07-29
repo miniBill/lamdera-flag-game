d70e15ad3b1efb1e98773268582a80055e3e639f
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-07-29 16:32:49 +0200

    Remove console.log

--- elm-pkg-js/localStorage.js
+++ elm-pkg-js/localStorage.js
@@ -1,16 +1,13 @@
 exports.init = function (app) {
   app.ports.loadLocalStorage.subscribe(function (key) {
     let value = getLocalStorage(key);
-    console.log(`loadLocalStorage(${key}) = ${value}`);
     app.ports.loadedLocalStorage.send({ key, value });
   });
   app.ports.saveToLocalStorage.subscribe(function ({ key, value }) {
-    console.log(`saveToLocalStorage({key:${key},value:${value}})`);
     localStorage.setItem(key, JSON.stringify(value));
   });
   app.ports.loadLocale.subscribe(function (key) {
     let value = getLocalStorage(key) || navigator.language;
-    console.log(`loadLocale(${key}) = ${value}`);
     app.ports.loadedLocalStorage.send({ key, value });
   });
 };
